8613771954c6853615d458e77fcdf43524972064
Mark Mann <mann.brinson@gmail.com>
2021-03-29 14:30:39 -0700

    youtube player loads dynamically

--- src/app/components/moviepage/moviepage.component.ts
+++ src/app/components/moviepage/moviepage.component.ts
@@ -2,19 +2,21 @@ import { Component, OnChanges, OnInit } from '@angular/core';
 import { MoviepageService } from './moviepage.service';
 import { ActivatedRoute } from "@angular/router";
 
+import { VideoItem } from './videoItem';
+
 @Component({
   selector: 'app-moviepage',
   templateUrl: './moviepage.component.html',
   styleUrls: ['./moviepage.component.css']
 })
-// export class MoviepageComponent implements OnInit {
+
 export class MoviepageComponent {
 
-  // public message: string = "Passing the data !!!"; //Can give it any type obj, etc.
-  public test: object = {"test": 1, "apple": 2};
-  // public movie_id: string = "";
-
   holder = {};
+  public preview_video: any = {};
+  // public preview_video: VideoItem = new(...);
+  // public preview_video = {} as VideoItem;
+
   constructor(
     private moviepageService: MoviepageService,
     private route: ActivatedRoute) { }
@@ -26,11 +28,14 @@ export class MoviepageComponent {
 
     // Will trigger whenver the tmdb_id in route changes
     this.route.params.subscribe(routeParams => {
-      console.log({"new tmdb_id": routeParams.tmdb_id})
+      // console.log({"new tmdb_id": routeParams.tmdb_id})
 
       this.moviepageService.getMoviepage(routeParams.tmdb_id!)
       .subscribe((data) => {
+
         this.holder = data;
+        this.preview_video = data.video;
+
       })  
     })
   }
--- src/app/components/youtube/youtube.component.ts
+++ src/app/components/youtube/youtube.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { VideoItem } from 'src/app/components/moviepage/videoItem';
 
 @Component({
   selector: 'app-youtube',
   templateUrl: './youtube.component.html',
   styleUrls: ['./youtube.component.css']
 })
-export class YoutubeComponent implements OnInit {
+export class YoutubeComponent {
+
+  @Input() video_data = {} as VideoItem;
 
   constructor() { }
 
-  ngOnInit() {
-    const tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
+  ngOnChanges() {
+    if ("key" in this.video_data) {
+      console.log("creating youtube vid");
+      console.log(this.video_data);
+      const tag = document.createElement('script');
+  
+      tag.src = "https://www.youtube.com/iframe_api";
+      document.body.appendChild(tag);
+    }
   }
 
 }
