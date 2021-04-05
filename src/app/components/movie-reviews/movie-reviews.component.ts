9cdb6610549619e5c85d100ad1a0f6b8317cc328
Mark Mann <mann.brinson@gmail.com>
2021-04-05 14:57:13 -0700

    movie page reviews rendering

--- src/app/components/movie-reviews/movie-reviews.component.ts
+++ src/app/components/movie-reviews/movie-reviews.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ReviewItem } from './reviewItem';
 
 @Component({
   selector: 'app-movie-reviews',
   templateUrl: './movie-reviews.component.html',
   styleUrls: ['./movie-reviews.component.css']
 })
-export class MovieReviewsComponent implements OnInit {
+export class MovieReviewsComponent {
+
+  @Input() reviews: Array<ReviewItem> = []
+
+  public len_reviews: number = 0
+  public reviews_empty_yn: boolean = true
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges() {
+    console.log({"reviews": this.reviews.length})
+    this.len_reviews = this.reviews.length
+
+    if (this.len_reviews > 0) {
+      this.reviews_empty_yn = false
+    }
+    
   }
 
 }
--- src/app/components/moviepage/moviepage.component.ts
+++ src/app/components/moviepage/moviepage.component.ts
@@ -42,42 +42,29 @@ export class MoviepageComponent {
   // ngOnInit() {
   ngOnInit() {
     var movie_id = this.route.snapshot.paramMap.get("tmdb_id");
-    // console.log({"paramMap": this.route.snapshot.paramMap});
-
-    // console.log({"current type": window.location.pathname.split("/")[2]});
 
     // Will trigger whenver the tmdb_id in route changes
     this.route.params.subscribe(routeParams => {
       console.log("arrived at this page")
 
-
-
-      // var entity_type = window.location.pathname.split("/")[2]
+      // Get entity_type
       this.entity_type = window.location.pathname.split("/")[2]
 
       this.moviepageService.getMoviepage(routeParams.tmdb_id!, this.entity_type)
       .subscribe((data) => {
-        // console.log({"current type": window.location.pathname.split("/")[2]});
-        // console.log("made it here")
-
+        // Assign data to class properties
         this.holder = data;
         this.preview_video = data.video;
         this.details = data.detail
         this.credits = data.credits
         this.reviews = data.reviews
 
+        // Shareable links
         this.youtube_page = encodeURIComponent(`https://www.youtube.com/watch?v=${this.preview_video.key}`)
         this.fb_share_page = `https://www.facebook.com/sharer/sharer.php?u=${this.youtube_page}`
-
-        // console.log({"name": encodeURI(this.details.name)})
         this.twitter_share_page = `https://twitter.com/intent/tweet?text=Watch%20${encodeURI(this.details.name)}%20${this.youtube_page}&hashtags=USC,CSCI571,FightOn`
 
-
-        // console.log({"encoded_youtube": encodeURIComponent(this.youtube_page)})
-        // console.log({"fb_share": this.fb_share_page})
-
-        console.log({"similar": data.similar})
-
+        //Recommendations
         this.similar = data.similar
         this.recommended = data.recommended
 
@@ -90,9 +77,6 @@ export class MoviepageComponent {
           this.recommended_empty_yn = false
         } else { this.recommended_empty_yn = true }
 
-        // console.log({"data": this.similar})
-        // console.log({"localstorage": this.localStorageService.localStorage})
-
         //// ADD TO LRU_CACHE HERE
         this.addMovieToLRU()
 
@@ -101,10 +85,7 @@ export class MoviepageComponent {
   }
 
   addMovieToLRU() {
-    // console.log("checking LRU")
-    // console.log(this.details)
-
-    ///PARAMETER
+    ///PARAMETERS
     var max_lru_length = 2 
 
     var item_to_add = {"id": this.details.tmdb_id,
@@ -112,15 +93,10 @@ export class MoviepageComponent {
                         "entity_type": this.entity_type,
                         "poster_path": this.details.poster_path
                       }
-    
     var lru_cache = this.localStorageService.get("lru_cache")
 
     //// LRU EXISTS
     if (lru_cache != null) { 
-      // console.log("lru exists")
-      // console.log({"storage before": this.localStorageService.localStorage})
-      // console.log(lru_cache)
-
       // Check if the curret movie is in lru
       var i
       var idx = 1000
@@ -137,9 +113,6 @@ export class MoviepageComponent {
 
       //// MOVIE IN LRU
       if (result.length > 0) {
-        // console.log("movie exists in lru")
-        // console.log({"lru_cache_arr": JSON.parse(lru_cache)})
-        // var new_val = JSON.parse(lru_cache)
 
         //Pop movie from LRU
         //Add movie to left part of LRU
@@ -152,11 +125,9 @@ export class MoviepageComponent {
 
       //// MOVIE NOT IN LRU
       else {
-        // console.log("movie not in lru")
 
         //// LRU IS FULL
         if (lru_cache.length == max_lru_length) {
-          // console.log("lru is full")
           var new_val = lru_cache
           new_val.pop()
           new_val.splice(0, 0, item_to_add)
@@ -165,11 +136,8 @@ export class MoviepageComponent {
         }
         //// LRU IS NOT FULL
         else {
-          // console.log({"lru_cache": JSON.parse(lru_cache)})
-          // console.log({"result to add": item_to_add})
           var new_val = lru_cache
           new_val.splice(0, 0, item_to_add)
-          // console.log({"lru_cache_new": new_val})
 
           this.localStorageService.set("lru_cache", new_val)
         }
@@ -177,7 +145,6 @@ export class MoviepageComponent {
    
     //// LRU DOES NOT EXIST
     } else {
-      // console.log("lru doesn't exist ")
       var lru_cache_new = [{"id": this.details.tmdb_id,
                     "name": this.details.name,
                     "entity_type": this.entity_type,
