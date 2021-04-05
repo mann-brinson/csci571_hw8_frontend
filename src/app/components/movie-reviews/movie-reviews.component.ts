be4a9cd837bf6c909e5870fa5076b2fcabf28b6a
Mark Mann <mann.brinson@gmail.com>
2021-04-05 16:47:01 -0700

    more movie reviews styling

--- src/app/components/movie-reviews/movie-reviews.component.ts
+++ src/app/components/movie-reviews/movie-reviews.component.ts
@@ -12,6 +12,7 @@ export class MovieReviewsComponent {
 
   public len_reviews: number = 0
   public reviews_empty_yn: boolean = true
+  public written_by_datetime: string[] = []
 
   constructor() { }
 
@@ -21,6 +22,26 @@ export class MovieReviewsComponent {
 
     if (this.len_reviews > 0) {
       this.reviews_empty_yn = false
+      
+      this.reviews.forEach((review) => {
+        // var date_raw = review.created_at.split('T')[0]
+        var date_raw = new Date(review.created_at)
+        var date_am_pm = ""
+        var hours_0 = date_raw.getHours()
+        var hours_1 = hours_0 % 12
+
+        if (Math.floor(hours_0/12) == 1) {date_am_pm = "PM"}
+        else {date_am_pm = "AM"}
+        
+        let formatted_date = `${date_raw.toLocaleString('default', { month: 'long' })} ${date_raw.getDate()}, ${date_raw.getFullYear()}, ${hours_1}:${date_raw.getMinutes()}:${date_raw.getSeconds()} ${date_am_pm}`
+
+        console.log({"review date": formatted_date})
+        // console.log(date_raw.get)
+        this.written_by_datetime.push(formatted_date)
+
+      })
+
+
     }
     
   }
